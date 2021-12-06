1b366a85b7a8e4e2206b911092eb6a660357d9ed
Kevin Konczak <konczakkevin@gmail.com>
2021-12-06 17:22:38 +0900

    Added function to rename page title and added comments

--- js/hiragana.js
+++ js/hiragana.js
@@ -11,12 +11,17 @@ Array.from(elements).forEach(function(element) {
 const urls = document.querySelectorAll(".pen");
 urls.forEach(function(item) {
   item.addEventListener('click', function() {
+    // Gets the name of the svg file
     const svg = localStorage.setItem('svgID', this.dataset.svgid);
-    console.log(svg);
+    // Gets the name of the hiragana
+    const reading = localStorage.setItem('reading', this.dataset.reading);
   });
 });
 
 function getSvgId() {
   const svgID = localStorage.getItem('svgID');
+  // adds the svg name to the src
   document.querySelector('.hiragana-stroke').src=`../svgsKana/${svgID}.svg`;
+  // Sets the page titke to the right hiragana reading
+  document.title = `Hiragana: ${localStorage.getItem('reading')}`;
 }
