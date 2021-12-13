c0bff3fcdccfe69247dca2696db911f05ea0b08e
Kevin Konczak <konczakkevin@gmail.com>
2021-12-13 15:41:50 +0900

    Created function to switch between tables. Moved color control to be standalone

--- js/hiragana.js
+++ js/hiragana.js
@@ -25,3 +25,10 @@ function getSvgId() {
   // Sets the page titke to the right hiragana reading
   document.title = `Hiragana: ${localStorage.getItem('reading')}`;
 }
+
+const switchButton = document.getElementById("kana-toggle");
+switchButton.addEventListener("click", function() {
+  switchButton.innerText == "Hiragana" ? switchButton.innerText = "Katakana" : switchButton.innerText = "Hiragana";
+  document.getElementById("hiragana").classList.toggle("d-none");
+  document.getElementById("katakana").classList.toggle("d-none");
+})
